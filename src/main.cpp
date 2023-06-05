fc5ea9943b64d5d1416869abcdafd067e8877e0f
Vov11x <vov18x@gmail.com>
2023-06-05 10:35:35 +0300

    Update shifts

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char** argv)
     int shmKey = 12345;
     gst_init(nullptr, nullptr);
 
-	auto camera = std::make_shared<streamer::CSICamera>(
+    auto camera = std::make_shared<streamer::CSICamera>(
         std::make_pair(2592, 1944),
         "NV12",
         30);
@@ -19,13 +19,13 @@ int main(int argc, char** argv)
     int shmSize = camera->GetResolution().first *
                   camera->GetResolution().second *
                   streamer::CSICamera::GetBytesPerPixel("GRAY8").value();
-	auto memory = std::make_shared<streamer::SharedMemory>(shmKey, shmSize);
-	auto videoStreamer = std::make_shared<streamer::VideoStreamer>(camera, memory);
+    auto memory = std::make_shared<streamer::SharedMemory>(shmKey, shmSize);
+    auto videoStreamer = std::make_shared<streamer::VideoStreamer>(camera, memory);
 	
     videoStreamer->StartCapturing();
-	std::cout << "[INFO] Capturing started!" << std::endl;
+    std::cout << "[INFO] Capturing started!" << std::endl;
 
     gst_deinit();
-	return 0;
+    return 0;
 }
 
